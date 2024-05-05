e1ac98c00c98431d218c05cd997a09e4c82c0816
balajipatil0009 <balajipatil20030@gmail.com>
2024-05-05 19:04:41 +0530

    Hover effect added to courses

--- src/componunts/CoursesOpns.jsx
+++ src/componunts/CoursesOpns.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const CoursesOpns = (props) => {
   const data = props.data;
   return (
-    <div className="mt-6 grid sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-4 gap-6 pb-3">
+    <div className="mt-6 grid sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-4 gap-6 pb-3 ">
       {data.map((item, index) => (
         <a href={item.path}>
           <div
             key={index}
-            className="border text-center border-gray-600 p-2 rounded-md min-w-[90vw] sm:min-w-56"
+            className="border text-center border-gray-600 p-2 rounded-md min-w-[90vw] sm:min-w-56 hover:bg-blue-500 hover:shadow-lg hover:shadow-blue-500/50 hover:border-none"
           >
             <h1>{item.name}</h1>
           </div>
