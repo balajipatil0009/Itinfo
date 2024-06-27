03cd64d2e94c1ce55b779fd6df55b897a0a6436a
balajipatil0009 <balajipatil20030@gmail.com>
2024-06-27 14:56:15 +0530

    resolving images renderring issue try1

--- src/pages/Gallary.jsx
+++ src/pages/Gallary.jsx
@@ -7,7 +7,7 @@ const Gallary = () => {
     <div className="w-[99vw] overflow-hidden">
       <Navbar />
       <div className="w-[99vw] overflow-hidden">
-        <div className="h-[100vh] bg-[url('./itinfo/info17.jpeg')] bg-center bg-cover flex justify-center items-center ">
+        <div className="h-[100vh] bg-[url('/itinfo/info17.jpeg')] bg-center bg-cover flex justify-center items-center ">
           <div className="h-full backdrop-blur-sm flex items-center">
             <h1 className="text-[10vw] font-Bank backdrop-blur-sm p-0 text-white">
               ITINFO ACADEMY
