e2a1550601c3cda8425948c5136f171c26300077
balajipatil0009 <balajipatil20030@gmail.com>
2024-07-02 15:08:38 +0530

    footer updated

--- src/componunts/Footer.jsx
+++ src/componunts/Footer.jsx
@@ -6,29 +6,34 @@ import { FaPhoneAlt } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className=" bg-amber-400 max-w-[100vw] font-sansLight">
-      <div className="flex justify-center">
-        <div>
-          <h1 className="text-center">Contact US</h1>
-          <div className="flex gap-5 w-fit">
-            <div className="flex items-center gap-2">
-              <BiLogoGmail className=" text-2xl" />
-              <h1 className="w-fit ">Itinfo@gmail.com</h1>
-            </div>
-            <div className="flex items-center gap-2 w-fit">
-              <FaPhoneAlt />
+    <div className=" bg-amber-400 max-w-[100vw] flex items-center h-auto justify-evenly font-sansLight">
+      <div>
+        <img src="./itinfo/help.jpeg" alt="help logo" className="h-[100px]" />
+      </div>
+      <div>
+        <div className="flex justify-center">
+          <div>
+            <h1 className="text-center">Contact US</h1>
+            <div className="flex gap-5 w-fit">
+              <div className="flex items-center gap-2">
+                <BiLogoGmail className=" text-2xl" />
+                <h1 className="w-fit ">Itinfo@gmail.com</h1>
+              </div>
+              <div className="flex items-center gap-2 w-fit">
+                <FaPhoneAlt />
 
-              <h1 className="w-fit">345678907890 / 345678909889</h1>
+                <h1 className="w-fit">345678907890 / 345678909889</h1>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div className="flex items-center w-[99vw] justify-center mt-6 text-sm">
-        <MdOutlineCopyright className=" text-gray-700" />
-        All rights are reserved .
-        <AiOutlineTrademark className=" text-gray-700" />
-        <h1 className=" text-gray-900"> ITINFO</h1>
+        <div className="flex items-center w-[70vw] justify-center mt-6 text-sm">
+          <MdOutlineCopyright className=" text-gray-700" />
+          All rights are reserved .
+          <AiOutlineTrademark className=" text-gray-700" />
+          <h1 className=" text-gray-900"> ITINFO</h1>
+        </div>
       </div>
     </div>
   );
